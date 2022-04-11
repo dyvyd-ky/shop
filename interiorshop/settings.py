361fef5b540b97ab67a6d9f29527560e7065e9a7
dyvyd-ky <john.ky.mlv@gmail.com>
2022-04-11 18:34:27 +0300

    com

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -191,7 +191,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'https://elyttes.nyc3.digitaloceanspaces.com'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
