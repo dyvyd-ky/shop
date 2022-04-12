214a89a6144dc5281c6fe93399dabeeee4ba7861
dyvyd-ky <john.ky.mlv@gmail.com>
2022-04-12 14:32:52 +0300

    vend

--- apps/order/models.py
+++ apps/order/models.py
@@ -31,7 +31,7 @@ class OrderItem(models.Model):
     
     
     def __str__(self):
-        return '%s' % self.id
+        return "{}, {}".format(self.vendor, self.id)
     
     def get_total_price(self):
         return self.price * self.quantity
