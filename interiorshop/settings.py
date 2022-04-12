19e1191e7cc8f71d65d0b03cf85bf69635201d27
dyvyd-ky <john.ky.mlv@gmail.com>
2022-04-12 10:43:04 +0300

    waiting

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -191,6 +191,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
+AWS_DEFAULT_ACL = 'public-read'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
