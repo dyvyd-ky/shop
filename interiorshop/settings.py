50c562bd9bd20c916567c2c33afd8192a3c562e1
dyvyd-ky <john.ky.mlv@gmail.com>
2022-04-12 12:54:16 +0300

    pls

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -197,7 +197,7 @@ AWS_LOCATION = 'elytteky'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_URL = '%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
