74d0bc3d9defc94f1702fddbee2281deb0302c1b
dyvyd-ky <john.ky.mlv@gmail.com>
2022-04-12 12:26:03 +0300

    rate

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -181,21 +181,24 @@ CKEDITOR_CONFIGS = {
     },
 }
 
-AWS_ACCESS_KEY_ID = 'WQH4YC7IYEETWQVZ2JLE'
-AWS_SECRET_ACCESS_KEY = 'FcvcF3DTImaoGws/Hp5uVuyxaFZ/42qaqw6bZkCtBNU'
+AWS_ACCESS_KEY_ID = '4OA4PHKRAQH2LQ4KHG7I'
+AWS_SECRET_ACCESS_KEY = 'LjbKr8NajlS1LJ7o6XrrAb1tr7mLYrqOMcyaxJcXVw8'
 
-AWS_STORAGE_BUCKET_NAME = 'elytteq'
-AWS_S3_ENDPOINT_URL = 'https://elytteq.sgp1.digitaloceanspaces.com'
+AWS_STORAGE_BUCKET_NAME = 'elytteky'
+AWS_S3_ENDPOINT_URL = 'https://elytteky.sgp1.digitaloceanspaces.com'
 AWS_S3_CUSTOM_DOMAIN = 'https://spaces.elytte.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
 AWS_DEFAULT_ACL = 'public-read'
-AWS_LOCATION = 'elytteq'
+AWS_LOCATION = 'elytteky'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-STATIC_ROOT = 'static/'
+
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
